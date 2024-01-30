784907480ed41037c8367555c5aecdaa44aaedb6
excelsior <jonathan.kila@jonathan.kila>
2024-01-30 15:42:48 +0000

    iframe protocol test

--- src/pages/NewReport/NewReport.jsx
+++ src/pages/NewReport/NewReport.jsx
@@ -1,14 +1,21 @@
-import "./NewReport.scss"
+import "./NewReport.scss";
 
 const NewReport = () => {
   return (
     <div class="new-report">
       <h1 class="new-report__heading">New Report</h1>
-      <form class="new-report__form" onsubmit={e => e.preventDefault()}>
+      <form
+        class="new-report__form pendo-ignore"
+        onsubmit={(e) => e.preventDefault()}>
         <label class="new-report__label" for="title">
           title
         </label>
-        <input class="new-report__input" type="text" name="title" />
+        <input
+          class="new-report__input pendo-ignore"
+          type="text"
+          name="title"
+          onchange={(e) => console.log(e.target.value)}
+        />
         <label class="new-report__label" for="body">
           body
         </label>
@@ -20,7 +27,7 @@ const NewReport = () => {
         <button class="new-report__button">submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewReport
+export default NewReport;
--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -1,23 +1,23 @@
-import ReportItem from "../../components/ReportItem/ReportItem"
-import { company } from "../../App"
-import reports from "../../data/reports"
-import "./Report.scss"
+import ReportItem from "../../components/ReportItem/ReportItem";
+import { company } from "../../App";
+import reports from "../../data/reports";
+import "./Report.scss";
 
 const Reports = () => {
   const companyReports = reports.filter(
-    report => report.companyId === company().id
-  )
+    (report) => report.companyId === company().id
+  );
 
   return (
     <div class="reports">
       <h1 class="reports__heading">Reports</h1>
       <ul class="reports__list">
         <For each={companyReports}>
-          {report => <ReportItem report={report} />}
+          {(report) => <ReportItem report={report} />}
         </For>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Reports
+export default Reports;
